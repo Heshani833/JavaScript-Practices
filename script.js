aa45c7af655fdc48ca4e8fc6c7027d3eb3727117
Heshani833 <heshanishehana123@gmail.com>
2025-02-11 22:33:34 +0530

    validation

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let hours = 27;
+function validation() {
+  let phone = document.getElementById("num").value;
 
-if (hours <= 12) {
-  console.log("Hello,Good Morning");
-} else if (hours < 24) {
-  console.log("Hello,Good Afternoon");
-} else {
-  console.log("Invalid hours");
+  let numLength = phone.length;
+  let firstNums = phone.substr(0, 2);
+  let lastNums = phone.substr(numLength - 9, 9);
+  let num;
+
+  if (phone.length < 9) {
+    alert("Numbers missing please check again.");
+    document.getElementById("msg").innerHTML = "Invalid Number";
+  } else if (phone.length == 9) {
+    alert("Number validation sucessful.");
+    num = "94" + phone;
+    document.getElementById("msg").innerHTML = num;
+  } else if (phone.length == 10) {
+    num = "94" + lastNums;
+    document.getElementById("msg").innerHTML = num;
+  }
 }
