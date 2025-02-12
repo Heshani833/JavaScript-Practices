155ad1b6fcd1e0cf7def351094fca97dda591c55
Heshani833 <heshanishehana123@gmail.com>
2025-02-12 22:11:01 +0530

    Validation 2

--- script.js
+++ script.js
@@ -15,6 +15,20 @@ function validation() {
     document.getElementById("msg").innerHTML = num;
   } else if (phone.length == 10) {
     num = "94" + lastNums;
+    alert("Number validation successful");
     document.getElementById("msg").innerHTML = num;
+  } else if (numLength > 11) {
+    alert("Invalid number , please check again");
+    document.getElementById("msg").innerHTML = "Invalid number..";
+  } else if (numLength == 11 && firstNums != 94) {
+    alert("Invalid number,please check again ");
+    document.getElementById("msg").innerHTML = "Invalid number..";
+  } else if (numLength == 11 && firstNums == 94) {
+    alert("Number validation sucessful.");
+    num = phone;
+    document.getElementById("msg").innerHTML = num;
+  } else {
+    alert("Invalid number,please check again ");
+    document.getElementById("msg").innerHTML = "Invalid number..";
   }
 }
