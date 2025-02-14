1eb79c89c3cd826646d3755a81a5c684f7e3c1d5
Heshani833 <heshanishehana123@gmail.com>
2025-02-14 23:15:09 +0530

    Switch + day

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let x = 3;
+console.log(new Date().getDay());
 
-switch (x) {
+let day;
+switch (new Date().getDay()) {
   case 0:
-    console.log("answer is 0");
+    day = "Sunday";
     break;
   case 1:
-    console.log("answer is 1");
+    day = "Monnday";
     break;
   case 2:
-    console.log("answer is 2");
+    day = "Tuesday";
     break;
   case 3:
-    console.log("answer is 3");
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thrusday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
     break;
 }
+
+document.getElementById("today").innerHTML = "Today is " + day;
