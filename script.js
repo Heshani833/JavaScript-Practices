bf9d4b78e0acf6f401c0df3cdc3fa40dc3ca47a2
Heshani833 <heshanishehana123@gmail.com>
2025-02-15 23:04:04 +0530

    Create objects

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-console.log(new Date().getDay());
+let student = { firstName: "Heshani", lastName: "Shehana", age: 23 };
+student.firstName = "Sachini";
 
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monnday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thrusday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-    break;
-}
-
-document.getElementById("today").innerHTML = "Today is " + day;
+document.getElementById("main").innerHTML =
+  "Hi, My name is" +
+  " " +
+  student.firstName +
+  " " +
+  student.lastName +
+  ". " +
+  student.age +
+  "years old .";
