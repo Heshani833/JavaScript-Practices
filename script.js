28fb0faa527672cf65afe1b08cae5b7a1114523f
Heshani833 <heshanishehana123@gmail.com>
2025-02-16 22:27:20 +0530

    Logic Gates

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let a = 2;
-a += 5;
-console.log(a);
-
-let b = "Heshani ";
-b += "Shehana";
-console.log(b);
-
-let z = 2;
-if (z == 2) {
-  console.log("done");
+let age = "25";
+let nationality = "SL";
+if (age < 20 || age > 30) {
+  console.log("You can't enter.");
+} else if (nationality == "SL") {
+  console.log("You are eligible.");
+} else {
+  console.log("You can't enter.");
 }
 
-
+let zcore = "1.4";
+let city = "Badulla";
+if (zcore > 1.5 && city > Badulla) {
+  console.log("You are eligible.");
+} else {
+  console.log("You can't enter.");
+}
