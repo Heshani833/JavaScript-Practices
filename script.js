00e815eb93d95bf25bac12ea697b8391cb2ec3d6
Heshani833 <heshanishehana123@gmail.com>
2025-02-16 22:43:35 +0530

    querySelector()

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-let age = "25";
-let nationality = "SL";
-if (age < 20 || age > 30) {
-  console.log("You can't enter.");
-} else if (nationality == "SL") {
-  console.log("You are eligible.");
-} else {
-  console.log("You can't enter.");
-}
-
-let zcore = "1.4";
-let city = "Badulla";
-if (zcore > 1.5 && city > Badulla) {
-  console.log("You are eligible.");
-} else {
-  console.log("You can't enter.");
-}
+document.querySelector("p.test").style.backgroundColor = "blue";
+document.querySelector("#main").innerHTML =
+  "Fruits <ul><li>Apple</li><li>Orange</li></ul>";
